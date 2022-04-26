9c39111a9f973e335a55ead44f2be76f50afbf02
Lilian Gallon <lilian.gallon@4sh.fr>
2022-04-26 15:43:28 +0200

    chore: (ocpi-2.1.1) Finished implementing LocationsEmspServer.kt

--- ocpi-lib-2.1.1/src/main/kotlin/ocpi/locations/LocationsEmspServer.kt
+++ ocpi-lib-2.1.1/src/main/kotlin/ocpi/locations/LocationsEmspServer.kt
@@ -1,6 +1,7 @@
 package ocpi.locations
 
 import common.mapper
+import ocpi.locations.domain.*
 import transport.TransportServer
 import transport.domain.*
 
@@ -10,7 +11,7 @@ import transport.domain.*
  */
 class LocationsEmspServer(
     private val transportServer: TransportServer,
-    private val callbacks: LocationsEmspInterface
+    private val controller: LocationsEmspInterface
 ) {
 
     init {
@@ -23,7 +24,7 @@ class LocationsEmspServer(
                 VariablePathSegment("locationId")
             )
         ) { req ->
-            val location = callbacks.getLocation(
+            val location = controller.getLocation(
                 countryCode = req.pathParams["countryCode"]!!,
                 partyId = req.pathParams["partyId"]!!,
                 locationId = req.pathParams["locationId"]!!
@@ -35,6 +36,196 @@ class LocationsEmspServer(
             )
         }
 
-        // TODO
+        transportServer.handle(
+            HttpMethod.GET,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId"),
+                VariablePathSegment("evseUid")
+            )
+        ) { req ->
+            val evse = controller.getEvse(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                evseUid = req.pathParams["evseUid"]!!
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(evse)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.GET,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId"),
+                VariablePathSegment("evseUid"),
+                VariablePathSegment("connectorId")
+            )
+        ) { req ->
+            val connector = controller.getConnector(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                evseUid = req.pathParams["evseUid"]!!,
+                connectorId = req.pathParams["connectorId"]!!
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(connector)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.PUT,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId")
+            )
+        ) { req ->
+            val location = controller.putLocation(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                location = mapper.readValue(req.body!!, Location::class.java)
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(location)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.PUT,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId"),
+                VariablePathSegment("evseUid")
+            )
+        ) { req ->
+            val evse = controller.putEvse(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                evseUid = req.pathParams["evseUid"]!!,
+                evse = mapper.readValue(req.body!!, Evse::class.java)
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(evse)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.PUT,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId"),
+                VariablePathSegment("evseUid"),
+                VariablePathSegment("connectorId")
+            )
+        ) { req ->
+            val connector = controller.putConnector(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                evseUid = req.pathParams["evseUid"]!!,
+                connectorId = req.pathParams["connectorId"]!!,
+                connector = mapper.readValue(req.body!!, Connector::class.java)
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(connector)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.PATCH,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId")
+            )
+        ) { req ->
+            val location = controller.patchLocation(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                location = mapper.readValue(req.body!!, LocationPatch::class.java)
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(location)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.PATCH,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId"),
+                VariablePathSegment("evseUid")
+            )
+        ) { req ->
+            val evse = controller.patchEvse(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                evseUid = req.pathParams["evseUid"]!!,
+                evse = mapper.readValue(req.body!!, EvsePatch::class.java)
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(evse)
+            )
+        }
+
+        transportServer.handle(
+            HttpMethod.PATCH,
+            listOf(
+                FixedPathSegment("/ocpi/emsp/2.0"),
+                VariablePathSegment("countryCode"),
+                VariablePathSegment("partyId"),
+                VariablePathSegment("locationId"),
+                VariablePathSegment("evseUid"),
+                VariablePathSegment("connectorId")
+            )
+        ) { req ->
+            val connector = controller.patchConnector(
+                countryCode = req.pathParams["countryCode"]!!,
+                partyId = req.pathParams["partyId"]!!,
+                locationId = req.pathParams["locationId"]!!,
+                evseUid = req.pathParams["evseUid"]!!,
+                connectorId = req.pathParams["connectorId"]!!,
+                connector = mapper.readValue(req.body!!, ConnectorPatch::class.java)
+            )
+
+            HttpResponse(
+                status = 200,
+                body = mapper.writeValueAsString(connector)
+            )
+        }
     }
 }
