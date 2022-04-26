6476b5f9e8b7a84f45f4901c5193dc1d6d58fcb8
Lilian Gallon <lilian.gallon@4sh.fr>
2022-04-26 11:09:13 +0200

    chore: (ocpi-2.1.1) Fixed Http4kTransportServer.kt implementation not taking into account path params

--- ocpi-lib-2.1.1/src/test/kotlin/samples/Http4kTransportServer.kt
+++ ocpi-lib-2.1.1/src/test/kotlin/samples/Http4kTransportServer.kt
@@ -1,32 +1,35 @@
 package samples
 
-import org.http4k.contract.ContractRoute
-import org.http4k.contract.bindContract
-import org.http4k.contract.contract
 import org.http4k.core.*
 import org.http4k.filter.DebuggingFilters
+import org.http4k.routing.RoutingHttpHandler
+import org.http4k.routing.bind
 import org.http4k.routing.path
+import org.http4k.routing.routes
 import org.http4k.server.Netty
 import org.http4k.server.asServer
 import transport.TransportServer
 import transport.domain.HttpMethod
 import transport.domain.HttpResponse
+import transport.domain.PathSegment
 
 class Http4kTransportServer(
     private val port: Int
 ) : TransportServer() {
 
-    private val serverRoutes: MutableList<ContractRoute> = mutableListOf()
+    private val serverRoutes: MutableList<RoutingHttpHandler> = mutableListOf()
 
     override fun handle(
         method: HttpMethod,
-        path: String,
-        pathParams: List<String>,
+        path: List<PathSegment>,
         queryParams: List<String>,
         callback: (pathParams: Map<String, String>, queryParams: Map<String, String?>) -> HttpResponse
     ) {
+        val pathParams = path.filter { it.param }.map { it.path }
+        val route = path.joinToString("/") { if (it.param) "{${it.path}}" else it.path }
+
         serverRoutes.add(
-            path bindContract Method.valueOf(method.name) to { req: Request ->
+            route bind Method.valueOf(method.name) to { req: Request ->
                 callback(
                     pathParams.associateWith { param -> req.path(param)!! },
                     queryParams.associateWith { param -> req.query(param) },
@@ -40,7 +43,7 @@ class Http4kTransportServer(
     override fun start() {
         DebuggingFilters.PrintRequestAndResponse()
             .then(
-                contract { routes += serverRoutes }
+                serverRoutes.reduce { a, b -> routes(a, b) }
             )
             .asServer(Netty(port)).start()
     }
