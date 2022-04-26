386f86d7de9303b584a9ca54d04106bd2afd0bc4
Lilian Gallon <lilian.gallon@4sh.fr>
2022-04-26 16:49:00 +0200

    chore: (ocpi-2.1.1) Added test libs & valiktor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,4 +2,7 @@ object Versions {
     const val http4k = "4.25.10.1"
     const val jackson = "2.13.2"
     const val logback = "1.3.0-alpha14"
+    const val valiktor = "0.12.0"
+    const val junit = "5.8.2"
+    const val strikt = "0.34.0"
 }
--- ocpi-lib-2.1.1/build.gradle.kts
+++ ocpi-lib-2.1.1/build.gradle.kts
@@ -6,11 +6,22 @@ dependencies {
     implementation(project(":transport"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${Versions.jackson}")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${Versions.jackson}")
+    implementation("org.valiktor:valiktor-core:${Versions.valiktor}")
 
     testImplementation(http4k("core"))
     testImplementation(http4k("contract"))
     testImplementation(http4k("client-jetty"))
     testImplementation(http4k("server-netty"))
 
+    testImplementation("org.junit.jupiter:junit-jupiter:${Versions.junit}")
+    testImplementation("io.strikt:strikt-core:${Versions.strikt}")
+
     testRuntimeOnly("ch.qos.logback:logback-classic:${Versions.logback}")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
