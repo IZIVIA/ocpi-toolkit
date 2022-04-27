e852a50badfb34cf805eec1f20461da4f825d2d1
Lilian Gallon <lilian.gallon@4sh.fr>
2022-04-27 11:23:13 +0200

    chore: (ocpi-2.1.1) Implemented extension function to create SearchResult

--- ocpi-lib-2.1.1/src/main/kotlin/common/SearchResult.kt
+++ ocpi-lib-2.1.1/src/main/kotlin/common/SearchResult.kt
@@ -6,3 +6,10 @@ data class SearchResult<T>(
     val limit: Int,
     val offset: Int
 )
+
+fun <T> List<T>.toSearchResult(totalCount: Int, limit: Int, offset: Int) = SearchResult(
+    list = this,
+    totalCount = totalCount,
+    limit = limit,
+    offset = offset
+)
