75190d9306352b179eca0fe1a0d6b2b86bcaa004
Lilian Gallon <lilian.gallon@4sh.fr>
2022-04-28 10:26:36 +0200

    chore: (ocpi-2.1.1) Renamed LocationsRepository.kt

--- ocpi-lib-2.1.1/src/main/kotlin/ocpi/locations/repositories/LocationsCpoRepository.kt
+++ ocpi-lib-2.1.1/src/main/kotlin/ocpi/locations/repositories/LocationsCpoRepository.kt
@@ -6,7 +6,7 @@ import ocpi.locations.domain.Evse
 import ocpi.locations.domain.Location
 import java.time.Instant
 
-interface LocationsRepository {
+interface LocationsCpoRepository {
     fun getLocations(dateFrom: Instant?, dateTo: Instant?, offset: Int = 0, limit: Int?): SearchResult<Location>
     fun getLocation(locationId: String): Location?
     fun getEvse(locationId: String, evseUid: String): Evse?
