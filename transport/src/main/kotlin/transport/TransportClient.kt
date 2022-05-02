fde955de3c91e4cb5d539d4d3869c34022ee12e7
Elian ORIOU <elian.oriou@4sh.fr>
2022-05-02 12:18:50 +0200

    chore - added comments

--- transport/src/main/kotlin/transport/TransportClient.kt
+++ transport/src/main/kotlin/transport/TransportClient.kt
@@ -3,8 +3,12 @@ package transport
 import transport.domain.HttpRequest
 import transport.domain.HttpResponse
 
-
-// TODO: documentation
 abstract class TransportClient {
+
+    /**
+     * Send a provided request to a server using a particular transport layer
+     * @param request the request to be sent to a server through the transport layer
+     * @return the response sent by the server
+     */
     abstract fun send(request: HttpRequest): HttpResponse
 }
