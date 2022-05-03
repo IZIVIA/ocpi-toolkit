8656569f83b9be620f7ef4e98808641076ec373e
Lilian Gallon <lilian.gallon@4sh.fr>
2022-05-03 18:09:40 +0200

    chore: (ocpi-2.2.1) Improved OCPI exceptions

--- ocpi-lib-2.2.1/src/main/kotlin/common/OcpiExceptions.kt
+++ ocpi-lib-2.2.1/src/main/kotlin/common/OcpiExceptions.kt
@@ -2,71 +2,83 @@ package common
 
 import transport.domain.HttpStatus
 
+sealed class OcpiException(
+    message: String,
+    open val httpStatus: HttpStatus,
+    open val ocpiStatus: OcpiStatus
+): Exception(message)
+
 // 2xxx: Client errors
 class OcpiClientGenericException(
     message: String,
-    val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
-    val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_ERROR
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_ERROR
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiClientInvalidParametersException(
     message: String = "Invalid or missing parameters",
-    val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
-    val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_INVALID_PARAMETERS
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_INVALID_PARAMETERS
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiClientNotEnoughInformationException(
     message: String = "Not enough information",
-    val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
-    val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_NOT_ENOUGH_INFORMATION
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_NOT_ENOUGH_INFORMATION
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiClientUnknownLocationException(
     message: String = "Unknown location",
-    val httpStatus: HttpStatus = HttpStatus.NOT_FOUND,
-    val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_UNKNOWN_LOCATION
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.NOT_FOUND,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_UNKNOWN_LOCATION
+) : OcpiException(message, httpStatus, ocpiStatus)
+
+class OcpiClientUnknownTokenException(
+    message: String = "Unknown token",
+    override val httpStatus: HttpStatus = HttpStatus.BAD_REQUEST,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.CLIENT_UNKNOWN_TOKEN
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 // 3xxx: Server errors
 class OcpiServerGenericException(
     message: String,
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_ERROR
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_ERROR
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiServerUnusableApiException(
     message: String = "Unable to use client's API",
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_UNUSABLE_API
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_UNUSABLE_API
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiServerUnsupportedVersionException(
     message: String = "Unsupported version",
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_UNSUPPORTED_VERSION
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_UNSUPPORTED_VERSION
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiServerNoMatchingEndpointsException(
     message: String = "No matching endpoints or expected endpoints missing between parties",
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_NO_MATCHING_ENDPOINTS
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.SERVER_NO_MATCHING_ENDPOINTS
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 // 4xxx: Hub errors
 class OcpiHubUnknownReceiverException(
     message: String = "Unknown receiver",
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.HUB_UNKNOWN_RECEIVER
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.HUB_UNKNOWN_RECEIVER
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiHubTimeoutOnRequestException(
     message: String = "Timeout on forwarded request",
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.HUB_REQUEST_TIMEOUT
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.HUB_REQUEST_TIMEOUT
+) : OcpiException(message, httpStatus, ocpiStatus)
 
 class OcpiHubConnectionProblemException(
     message: String = "Connection problem",
-    val httpStatus: HttpStatus = HttpStatus.OK,
-    val ocpiStatus: OcpiStatus = OcpiStatus.HUB_CONNECTION_PROBLEM
-) : Exception(message)
+    override val httpStatus: HttpStatus = HttpStatus.OK,
+    override val ocpiStatus: OcpiStatus = OcpiStatus.HUB_CONNECTION_PROBLEM
+) : OcpiException(message, httpStatus, ocpiStatus)
