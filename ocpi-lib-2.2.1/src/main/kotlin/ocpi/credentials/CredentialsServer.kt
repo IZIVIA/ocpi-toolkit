561c81e9905837863e9800697d47ace1435e2911
Lilian Gallon <lilian.gallon@4sh.fr>
2022-05-04 18:35:36 +0200

    chore: (ocpi-2.2.1) Implemented CredentialsServer (only post for now)

--- ocpi-lib-2.2.1/src/main/kotlin/ocpi/credentials/CredentialsServer.kt
+++ ocpi-lib-2.2.1/src/main/kotlin/ocpi/credentials/CredentialsServer.kt
@@ -1,3 +1,39 @@
 package ocpi.credentials
 
-class CredentialsServer
+import common.OcpiClientNotEnoughInformationException
+import common.decodeBase64
+import common.httpResponse
+import common.mapper
+import ocpi.credentials.domain.Credentials
+import ocpi.credentials.services.CredentialsServerService
+import transport.TransportServer
+import transport.domain.FixedPathSegment
+import transport.domain.HttpMethod
+
+class CredentialsServer(
+    transportServer: TransportServer,
+    service: CredentialsServerService
+) {
+    init {
+        // TODO: get
+
+        transportServer.handle(
+            method = HttpMethod.POST,
+            path = listOf(
+                FixedPathSegment("/credentials")
+            )
+        ) { req -> httpResponse {
+                service.post(
+                    tokenA = req.headers["Authorization"]
+                        ?.removePrefix("Token ")
+                        ?.decodeBase64()
+                        ?: throw OcpiClientNotEnoughInformationException("Missing Authorization header"),
+                    credentials = mapper.readValue(req.body!!, Credentials::class.java)
+                )
+            }
+        }
+
+        // TODO: put
+        // TODO: delete
+    }
+}
---
+++ ocpi-lib-2.2.1/src/main/kotlin/ocpi/credentials/services/CredentialsServerService.kt
@@ -0,0 +1,84 @@
+package ocpi.credentials.services
+
+import common.*
+import ocpi.credentials.CredentialsInterface
+import ocpi.credentials.domain.Credentials
+import ocpi.credentials.repositories.PlatformRepository
+import ocpi.versions.domain.Version
+import ocpi.versions.domain.VersionDetails
+import ocpi.versions.domain.VersionNumber
+import transport.TransportClientBuilder
+import transport.domain.HttpMethod
+import transport.domain.HttpRequest
+
+class CredentialsServerService(
+    private val platformRepository: PlatformRepository,
+    private val transportClientBuilder: TransportClientBuilder
+): CredentialsInterface {
+
+    override fun get() {
+        TODO("Not yet implemented")
+    }
+
+    override fun post(
+        tokenA: String,
+        credentials: Credentials
+    ): OcpiResponseBody<Credentials> = OcpiResponseBody.of {
+        val platform = platformRepository.getPlatformByTokenA(tokenA)
+            ?: throw OcpiClientInvalidParametersException("Invalid CREDENTIALS_TOKEN_A ($tokenA)")
+
+        val versions = transportClientBuilder
+            .build(credentials.url)
+            .send(
+                HttpRequest(
+                    method = HttpMethod.GET,
+                    path = "/",
+                    headers = mapOf(
+                        "Authorization" to "Token ${credentials.token.encodeBase64()}"
+                    )
+                )
+            )
+            .parseBody<OcpiResponseBody<List<Version>>>()
+            .data
+            ?: throw OcpiServerGenericException("Could not get versions of sender")
+
+        val matchingVersion = versions.firstOrNull { it.version == VersionNumber.V2_2_1 }
+            ?: throw OcpiServerNoMatchingEndpointsException("Expected version 2.2.1 from $versions")
+
+        val versionDetail = transportClientBuilder
+            .build(matchingVersion.url)
+            .send(
+                HttpRequest(
+                    method = HttpMethod.GET,
+                    path = "",
+                    headers = mapOf(
+                        "Authorization" to "Token ${credentials.token.encodeBase64()}"
+                    )
+                )
+            )
+            .parseBody<OcpiResponseBody<VersionDetails>>()
+            .data
+            ?: throw OcpiServerGenericException("Could not get versions of sender")
+
+        // TODO: Should we do something about roles ?
+
+        val tokenC = platformRepository.saveCredentialsTokenC(platformUrl = platform, credentialsTokenC = generateUUIDv4Token())
+        platformRepository.saveVersion(platformUrl = platform, version = matchingVersion)
+        platformRepository.saveEndpoints(platformUrl = platform, endpoints = versionDetail.endpoints)
+        platformRepository.removeCredentialsTokenA(platformUrl = platform)
+
+        Credentials(
+            token = tokenC,
+            url = "", // TODO: properties.url or something
+            roles = emptyList() // TODO: ???
+        )
+    }
+
+    override fun put() {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete() {
+        TODO("Not yet implemented")
+    }
+}
