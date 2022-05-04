3be9a6faf492f13d9b095f234bb637e45851b760
Lilian Gallon <lilian.gallon@4sh.fr>
2022-05-04 18:37:00 +0200

    chore: (ocpi-2.2.1) Implemented mongo mock of platform repository

---
+++ ocpi-lib-2.2.1/src/test/kotlin/tests/mock/PlatformRepository.kt
@@ -0,0 +1,57 @@
+package tests.mock
+
+import com.mongodb.client.MongoCollection
+import ocpi.credentials.repositories.PlatformRepository
+import ocpi.versions.domain.Endpoint
+import ocpi.versions.domain.Version
+import org.litote.kmongo.eq
+import org.litote.kmongo.findOne
+import org.litote.kmongo.set
+import org.litote.kmongo.setTo
+import samples.common.Platform
+
+class PlatformMongoRepository(
+    private val collection: MongoCollection<Platform>
+) : PlatformRepository {
+    override fun saveVersion(platformUrl: String, version: Version): Version = version.also {
+        collection
+            .updateOne(Platform::url eq platformUrl, set(Platform::version setTo it))
+    }
+
+    override fun saveEndpoints(platformUrl: String, endpoints: List<Endpoint>): List<Endpoint> = endpoints.also {
+        collection
+            .updateOne(Platform::url eq platformUrl, set(Platform::endpoints setTo it))
+    }
+
+    override fun saveCredentialsTokenB(platformUrl: String, credentialsTokenB: String): String =
+        credentialsTokenB.also {
+            collection
+                .updateOne(Platform::url eq platformUrl, set(Platform::tokenB setTo it))
+        }
+
+    override fun saveCredentialsTokenC(platformUrl: String, credentialsTokenC: String): String =
+        credentialsTokenC.also {
+            collection
+                .updateOne(Platform::url eq platformUrl, set(Platform::tokenC setTo it))
+        }
+
+    override fun removeCredentialsTokenA(platformUrl: String) {
+        collection
+            .updateOne(Platform::url eq platformUrl, set(Platform::tokenA setTo null))
+    }
+
+    override fun getCredentialsTokenA(platformUrl: String): String? = collection
+        .findOne(Platform::url eq platformUrl)?.tokenA
+
+    override fun getCredentialsTokenC(platformUrl: String): String? = collection
+        .findOne(Platform::url eq platformUrl)?.tokenC
+
+    override fun getPlatformByTokenA(token: String): String? = collection
+        .findOne(Platform::tokenA eq token)?.url
+
+    override fun getPlatformByTokenB(token: String): String? = collection
+        .findOne(Platform::tokenB eq token)?.url
+
+    override fun getPlatformByTokenC(token: String): String? = collection
+        .findOne(Platform::tokenC eq token)?.url
+}
