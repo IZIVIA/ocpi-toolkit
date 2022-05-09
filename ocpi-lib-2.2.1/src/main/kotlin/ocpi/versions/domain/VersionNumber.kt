e01d5c11b19b89c4dcdcdb836ada1b132f61d06c
Lilian Gallon <lilian.gallon@4sh.fr>
2022-05-09 11:32:17 +0200

    chore: (ocpi-2.2.1) updated VersionNumber enum to match the specs

--- ocpi-lib-2.2.1/src/main/kotlin/ocpi/versions/domain/VersionNumber.kt
+++ ocpi-lib-2.2.1/src/main/kotlin/ocpi/versions/domain/VersionNumber.kt
@@ -5,6 +5,11 @@ package ocpi.versions.domain
  */
 enum class VersionNumber(val value: String, val index: Int) {
     /**
+     * OCPI version 2.2 (DEPRECATED, do not use, use 2.2.1 instead)
+     */
+    V2_2("2.2", 3),
+
+    /**
      * OCPI version 2.2.1 (this version)
      */
     V2_2_1("2.2.1", 4)
