4e617706622fb57c40ab138ba7cbdd948358f547
Lilian Gallon <lilian.gallon@4sh.fr>
2022-05-20 17:01:23 +0200

    chore: (transport / common) Added gradle publish task

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,29 +1,6 @@
 plugins {
     kotlin("jvm")
-    id("maven-publish")
 }
 
 dependencies {
-}
-
-val repo4shUser: String by project
-val repo4shPassword: String by project
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            artifactId = "ocpi-common"
-            from(components["java"])
-        }
-    }
-    repositories {
-        maven {
-            name = "libs-release-local"
-            url = uri("https://repo.4sh.fr/artifactory/libs-release-local")
-            credentials {
-                username = repo4shUser
-                password = repo4shPassword
-            }
-        }
-    }
 }
