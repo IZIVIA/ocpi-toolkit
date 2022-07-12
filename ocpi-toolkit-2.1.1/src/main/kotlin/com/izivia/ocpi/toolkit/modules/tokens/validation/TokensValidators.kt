18d7e5c07d32d14143c526baf2a3a1b241bff99c
Lilian Gallon <lilian.gallon@4sh.fr>
2022-07-12 17:59:57 +0200

    IDEV-130: implemented Tokens module CPO/eMSP validation services

--- ocpi-toolkit-2.1.1/src/main/kotlin/com/izivia/ocpi/toolkit/modules/tokens/validation/TokensValidators.kt
+++ ocpi-toolkit-2.1.1/src/main/kotlin/com/izivia/ocpi/toolkit/modules/tokens/validation/TokensValidators.kt
@@ -3,10 +3,11 @@ package com.izivia.ocpi.toolkit.modules.tokens.validation
 import com.izivia.ocpi.toolkit.common.validation.hasMaxLengthOf
 import com.izivia.ocpi.toolkit.common.validation.isLanguage
 import com.izivia.ocpi.toolkit.common.validation.isPrintableAscii
-import com.izivia.ocpi.toolkit.modules.tokens.domain.Token
-import com.izivia.ocpi.toolkit.modules.tokens.domain.TokenPartial
-import com.izivia.ocpi.toolkit.modules.tokens.domain.toPartial
+import com.izivia.ocpi.toolkit.modules.locations.validation.validate
+import com.izivia.ocpi.toolkit.modules.tokens.domain.*
 import org.valiktor.validate
+
+private data class StringWrapper(val str: String)
 
 fun TokenPartial.validate(): TokenPartial = validate(this) {
     validate(TokenPartial::uid).isPrintableAscii().hasMaxLengthOf(36)
@@ -18,6 +19,30 @@ fun TokenPartial.validate(): TokenPartial = validate(this) {
     // whitelist: nothing to validate
     validate(TokenPartial::language).isLanguage()
     // last_updated: : nothing to validate
+}
+
+fun LocationReferences.validate(): LocationReferences = validate(this) {
+    validate(LocationReferences::location_id)
+    evse_uids
+        .map { StringWrapper(it) }
+        .forEach {
+            validate(it) {
+                validate(StringWrapper::str).isPrintableAscii().hasMaxLengthOf(39)
+            }
+        }
+    connector_ids
+        .map { StringWrapper(it) }
+        .forEach {
+            validate(it) {
+                validate(StringWrapper::str).isPrintableAscii().hasMaxLengthOf(39)
+            }
+        }
+}
+
+fun AuthorizationInfo.validate(): AuthorizationInfo = validate(this) {
+    // allowed: nothing to validate
+    location?.validate()
+    info?.validate()
 }
 
 fun Token.validate(): Token = validate(this) {
