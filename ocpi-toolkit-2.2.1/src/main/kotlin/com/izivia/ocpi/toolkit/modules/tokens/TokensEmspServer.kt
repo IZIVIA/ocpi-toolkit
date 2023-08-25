5efacfaf515e42458e8f7c6291ba62da0b76374a
LBU <louis.baizeau@4sh.fr>
2023-08-25 14:53:12 +0200

    implement CPO and EMSP client/server of token module

---
+++ ocpi-toolkit-2.2.1/src/main/kotlin/com/izivia/ocpi/toolkit/modules/tokens/TokensEmspServer.kt
@@ -0,0 +1,64 @@
+package com.izivia.ocpi.toolkit.modules.tokens
+
+import com.izivia.ocpi.toolkit.common.OcpiModuleServer
+import com.izivia.ocpi.toolkit.common.httpResponse
+import com.izivia.ocpi.toolkit.common.mapper
+import com.izivia.ocpi.toolkit.modules.tokens.domain.LocationReferences
+import com.izivia.ocpi.toolkit.modules.tokens.domain.TokenType
+import com.izivia.ocpi.toolkit.transport.TransportServer
+import com.izivia.ocpi.toolkit.transport.domain.FixedPathSegment
+import com.izivia.ocpi.toolkit.transport.domain.HttpMethod
+import com.izivia.ocpi.toolkit.transport.domain.VariablePathSegment
+import java.time.Instant
+
+/**
+ * Receives calls from a CPO
+ * @property transportServer
+ */
+class TokensEmspServer(
+    private val service: TokensEmspInterface,
+    basePath: String = "/2.2.1/tokens"
+) : OcpiModuleServer(basePath) {
+    override fun registerOn(transportServer: TransportServer) {
+
+        //GET Token
+        transportServer.handle(
+            method = HttpMethod.GET,
+            path = basePathSegments,
+            queryParams = listOf("date_from", "date_to", "offset", "limit")
+        ) { req ->
+            req.httpResponse {
+                val dateFrom = req.queryParams["date_from"]
+                val dateTo = req.queryParams["date_to"]
+
+                service
+                    .getTokens(
+                        dateFrom = dateFrom?.let { Instant.parse(it) },
+                        dateTo = dateTo?.let { Instant.parse(it) },
+                        offset = req.queryParams["offset"]?.toInt() ?: 0,
+                        limit = req.queryParams["limit"]?.toInt()
+                    )
+            }
+        }
+
+        //POST token
+        transportServer.handle(
+            method = HttpMethod.POST,
+            path = basePathSegments + listOf(
+                VariablePathSegment("tokenUid"),
+                FixedPathSegment("authorize")
+            ),
+            queryParams = listOf("type")
+        ) { req ->
+                req.httpResponse {
+                    service.postToken(
+                        tokenUid = req.pathParams["tokenUid"]!!,
+                        type = req.queryParams["type"]?.let { enumValueOf<TokenType>(it) } ?: TokenType.RFID,
+                        locationReferences = req.body
+                            ?.takeIf { it.isNotBlank() } // During Test if client sent body = null, this reiceve body=""
+                            ?.let { mapper.readValue(it, LocationReferences::class.java) }
+                    )
+                }
+        }
+    }
+}
