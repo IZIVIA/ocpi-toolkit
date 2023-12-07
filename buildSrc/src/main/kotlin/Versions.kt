fd227000db22cc2f00f619f44458e2857356b117
Lilian Gallon <lilian.gallon@4sh.fr>
2023-12-07 16:35:02 +0100

    chore(gradle): bump libs

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,22 @@
 object Versions {
-    const val http4k = "4.25.10.1"
-    const val jackson = "2.13.2"
-    const val logback = "1.3.0-alpha14"
+    // Used by core
+    const val jackson = "2.16.0"
+    const val log4j = "2.22.0"
     const val valiktor = "0.12.0"
-    const val junit = "5.8.2"
-    const val strikt = "0.34.0"
-    const val kotlinPoet = "1.14.2"
-    const val mockk = "1.12.3"
-    const val log4j = "2.17.2"
-    const val testcontainers = "1.16.3"
-    const val kmongo = "4.5.1"
     const val coroutines = "1.7.3"
-    const val ksp = "1.9.10-1.0.13"
-    const val nexus = "1.1.0"
     const val kotlin = "1.9.10"
+
+    // Used to build / gradle
+    const val ksp = "1.9.10-1.0.13"
+    const val kotlinPoet = "1.15.3"
+    const val nexus = "1.3.0"
+
+    // Used only for tests
+    const val logback = "1.4.14"
+    const val junit = "5.10.1"
+    const val http4k = "4.48.0.0"
+    const val strikt = "0.34.1"
+    const val mockk = "1.13.8"
+    const val testcontainers = "1.19.3"
+    const val kmongo = "4.11.0"
 }
