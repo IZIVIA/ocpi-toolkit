80e4b034e2c03494685956d71063611b4616f409
Lilian Gallon <lilian.gallon@4sh.fr>
2024-06-19 13:32:44 +0200

    chore(gradle) #112: remove duplicated declarations & a-p / common module maven publishing

--- annotation-processor/build.gradle.kts
+++ annotation-processor/build.gradle.kts
@@ -2,42 +2,9 @@ plugins {
     kotlin("jvm")
 }
 
-repositories {
-    mavenCentral()
-}
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
-
-publishing {
-    publications {
-        named<MavenPublication>("maven") {
-            artifactId = "ocpi-annotation-processor"
-            groupId = "com.izivia"
-
-            from(components["java"])
-
-            pom {
-                name.set("OCPI Annotation processor")
-                artifactId = "ocpi-annotation-processor"
-                description.set(
-                    "This module processes annotations during compilation time in order to generate some code " +
-                        "(partial representation, etc ...)"
-                )
-            }
-        }
-    }
-}
-
 dependencies {
     implementation("com.squareup:kotlinpoet:${Versions.kotlinPoet}")
     implementation("com.squareup:kotlinpoet-ksp:${Versions.kotlinPoet}")
     implementation("com.google.devtools.ksp:symbol-processing-api:${Versions.ksp}")
     implementation(project(":common"))
-}
-
-tasks.build {
-    dependsOn(tasks.publishToMavenLocal)
 }
