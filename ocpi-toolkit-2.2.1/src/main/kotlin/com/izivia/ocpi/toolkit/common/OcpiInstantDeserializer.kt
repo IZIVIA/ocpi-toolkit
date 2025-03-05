f0c83f6f75861741499b91af3c7e19a9965cbcd2
Lilian Gallon <lilian.gallon@4sh.fr>
2025-03-05 17:56:13 +0100

    fix(mapper): deserializer uses java built in instant parse and adds Z if it's missing
    The compatibility is better this way. Java built-in parse can parse the ocpi datetime, but did not support missing "Z" at the end

--- ocpi-toolkit-2.2.1/src/main/kotlin/com/izivia/ocpi/toolkit/common/OcpiInstantDeserializer.kt
+++ ocpi-toolkit-2.2.1/src/main/kotlin/com/izivia/ocpi/toolkit/common/OcpiInstantDeserializer.kt
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer
 import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneOffset
-import java.time.format.DateTimeFormatter
 
 class OcpiInstantDeserializer : StdDeserializer<Instant>(Instant::class.java) {
     override fun deserialize(parser: JsonParser, context: DeserializationContext): Instant {
-        val value = parser.text.trim()
-        val localDateTime = LocalDateTime.parse(value, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-        return localDateTime.atOffset(ZoneOffset.UTC).toInstant()
+        return Instant.parse(
+            parser.valueAsString.run {
+                if (!endsWith("Z")) {
+                    "${this}Z"
+                } else {
+                    this
+                }
+            },
+        )
     }
 }
